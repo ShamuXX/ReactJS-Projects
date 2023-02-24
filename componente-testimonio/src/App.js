1e1ba76b7f43efac8ac374b2af8ca8bf1e1e5e61
Samuel <samuel16mendoza@gmail.com>
2023-02-24 05:09:06 -0600

    First component was created whiout styles

---
+++ componente-testimonio/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CardTestimonio from "./components/CardTestimonio";
+
+function App() {
+  return (
+    <div className="App">
+      <CardTestimonio />
+    </div>
+  );
+}
+
+export default App;
