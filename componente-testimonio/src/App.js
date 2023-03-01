39412a33e7a6672ae2bbf4cfca507cb5d6c3e46c
Samuel <samuel16mendoza@gmail.com>
2023-02-28 21:42:13 -0600

    Styles was added to Card Component

--- componente-testimonio/src/App.js
+++ componente-testimonio/src/App.js
@@ -5,7 +5,40 @@ import CardTestimonio from "./components/CardTestimonio";
 function App() {
   return (
     <div className="App">
-      <CardTestimonio />
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      <div>
+        <CardTestimonio
+          nombre="Emma Bostian en Suecia"
+          cargo="Ingeniera de Software en Spotify"
+          foto="../Images/Emma-5a662db9bcf55809a1fefb2ea17634bc.png"
+          texto='"Siempre he tenido problemas para aprender JavaScript. He tomado
+        muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
+        Estudiar JavaScript, así como estructuras de datos y algoritmos en
+        freeCodeCamp me dio las habilidades y la confianza que necesitaba para
+        conseguir el trabajo de mis sueños como ingeniero de software en
+        Spotify."'
+        />
+        <CardTestimonio
+          nombre="Emma Bostian en Suecia"
+          cargo="Ingeniera de Software en Spotify"
+          texto='"Siempre he tenido problemas para aprender JavaScript. He tomado
+        muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
+        Estudiar JavaScript, así como estructuras de datos y algoritmos en
+        freeCodeCamp me dio las habilidades y la confianza que necesitaba para
+        conseguir el trabajo de mis sueños como ingeniero de software en
+        Spotify."'
+        />
+        <CardTestimonio
+          nombre="Emma Bostian en Suecia"
+          cargo="Ingeniera de Software en Spotify"
+          texto='"Siempre he tenido problemas para aprender JavaScript. He tomado
+        muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
+        Estudiar JavaScript, así como estructuras de datos y algoritmos en
+        freeCodeCamp me dio las habilidades y la confianza que necesitaba para
+        conseguir el trabajo de mis sueños como ingeniero de software en
+        Spotify."'
+        />
+      </div>
     </div>
   );
 }
--- componente-testimonio/src/components/CardTestimonio.js
+++ componente-testimonio/src/components/CardTestimonio.js
@@ -1,24 +1,17 @@
 import React from "react";
-
-function CardTestimonio() {
+import "../styles/CardTestimonio.css";
+function CardTestimonio(props) {
   return (
-    <div className="contenedor-testimonio">
+    <div className="card">
       <img
         className="imagen-testimonio"
         src={require("../Images/Emma-5a662db9bcf55809a1fefb2ea17634bc.png")}
         alt="Foto de Emma"
       />
       <div className="contenedor-texto">
-        <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-        <p className="cargo-testimonio">Ingeniera de Software en Spotify</p>
-        <p className="texto-testimonio">
-          "Siempre he tenido problemas para aprender JavaScript. He tomado
-          muchos cursos, pero el curso de freeCodeCamp fue el que se quedó.
-          Estudiar JavaScript, así como estructuras de datos y algoritmos en
-          freeCodeCamp me dio las habilidades y la confianza que necesitaba para
-          conseguir el trabajo de mis sueños como ingeniero de software en
-          Spotify."
-        </p>
+        <p className="nombre-testimonio">{props.nombre}</p>
+        <p className="cargo-testimonio">{props.cargo}</p>
+        <p className="texto-testimonio">{props.texto}</p>
       </div>
     </div>
   );
